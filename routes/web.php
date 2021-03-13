0946257f2240545c34b275dc4e8f195747109f40
Dusan <dstojnic9@gmail.com>
2021-03-13 12:27:11 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
     Route::get('code', [GoogleAuthController::class, 'code'])->name('code');
     Route::get('retrieve', [GoogleAuthController::class, 'retrieve'])->name('retrieve');
 });
+
+
